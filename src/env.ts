a12fa375d1821b440311bbe58279cb0b5041676a
Steve-0628 <github@hmpf.club>
2022-08-13 16:07:29 +0900

    add cookie stuff in env

--- src/env.ts
+++ src/env.ts
@@ -5,5 +5,12 @@ config()
 export default {
     CK: process.env.CK,
     CS: process.env.CS,
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.PORT ?? 3000,
+    COOKIE: {
+        HttpOnly: process.env.C_HTTPONLY ?? true,
+        Secure: process.env.C_SECURE ?? true,
+        SameSite: process.env.C_SAMESITE ?? "Strict",
+        Domain: process.env.C_DOMAIN,
+        MaxAge: process.env.C_MAXAGE ?? 60 * 60 * 24 * 365,
+    }
 }
