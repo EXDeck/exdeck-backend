e67296f8ba8bdebe2dc951ee60044f57c5023550
Steve-0628 <github@hmpf.club>
2022-08-13 16:13:15 +0900

    change param name

--- src/env.ts
+++ src/env.ts
@@ -6,11 +6,11 @@ export default {
     CK: process.env.CK,
     CS: process.env.CS,
     PORT: process.env.PORT ?? 3000,
-    COOKIE: {
-        HttpOnly: process.env.C_HTTPONLY ?? true,
-        Secure: process.env.C_SECURE ?? true,
-        SameSite: process.env.C_SAMESITE ?? "Strict",
-        Domain: process.env.C_DOMAIN,
-        MaxAge: process.env.C_MAXAGE ?? 60 * 60 * 24 * 365,
+    COOKIE: { //このオブジェクトはcookeisのオプションにそのまま突っ込めるようになってる
+        httpOnly: process.env.C_HTTPONLY ?? true,
+        secure: process.env.C_SECURE ?? true,
+        sameSite: process.env.C_SAMESITE ?? "Strict",
+        domain: process.env.C_DOMAIN,
+        maxAge: process.env.C_MAXAGE ?? 60 * 60 * 24 * 365,
     }
 }
