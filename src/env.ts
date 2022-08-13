946241e65190f5c51ebcf6cdfb9800e14e46b815
Steve-0628 <github@hmpf.club>
2022-08-13 16:27:53 +0900

    fix samesite type

--- src/env.ts
+++ src/env.ts
@@ -6,10 +6,10 @@ export default {
     CK: process.env.CK,
     CS: process.env.CS,
     PORT: process.env.PORT ?? 3000,
-    COOKIE: { //このオブジェクトはcookeisのオプションにそのまま突っ込めるようになってる←まだなってない
+    COOKIE: { //このオブジェクトはcookeisのオプションにそのまま突っ込めるようになってる
         httpOnly: process.env.C_HTTPONLY == undefined ? true : Boolean(process.env.C_HTTPONLY),
         secure: process.env.C_SECURE == undefined ? true : Boolean(process.env.C_SECURE),
-        sameSite: process.env.C_SAMESITE ?? "Strict",
+        sameSite: process.env.C_SAMESITE as "strict" | "lax" | "none" ?? "strict",
         domain: process.env.C_DOMAIN,
         maxAge: process.env.C_MAXAGE == undefined ?  60 * 60 * 24 * 365 : Number(process.env.C_MAXAGE),
     }
