eaf5686d9a6635a10e3402c92efb7b78aaf06ea6
Steve-0628 <github@hmpf.club>
2022-08-13 16:20:40 +0900

    fix boolean and number

--- src/env.ts
+++ src/env.ts
@@ -6,11 +6,11 @@ export default {
     CK: process.env.CK,
     CS: process.env.CS,
     PORT: process.env.PORT ?? 3000,
-    COOKIE: { //このオブジェクトはcookeisのオプションにそのまま突っ込めるようになってる
-        httpOnly: process.env.C_HTTPONLY ?? true,
-        secure: process.env.C_SECURE ?? true,
+    COOKIE: { //このオブジェクトはcookeisのオプションにそのまま突っ込めるようになってる←まだなってない
+        httpOnly: process.env.C_HTTPONLY == undefined ? true : Boolean(process.env.C_HTTPONLY),
+        secure: process.env.C_SECURE == undefined ? true : Boolean(process.env.C_SECURE),
         sameSite: process.env.C_SAMESITE ?? "Strict",
         domain: process.env.C_DOMAIN,
-        maxAge: process.env.C_MAXAGE ?? 60 * 60 * 24 * 365,
+        maxAge: process.env.C_MAXAGE == undefined ?  60 * 60 * 24 * 365 : Number(process.env.C_MAXAGE),
     }
 }
