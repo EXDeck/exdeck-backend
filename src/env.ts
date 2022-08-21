3fd06b8cf368ba980d290aa02dc8f995349c6d60
すえ <souhait.0614@gmail.com>
2022-08-21 19:15:09 +0900

    envのboolean判定処理を変更

--- src/env.ts
+++ src/env.ts
@@ -2,14 +2,18 @@ import {config} from "dotenv"
 
 config()
 
+function boolean(boolStr: string) {
+    return boolStr.toLowerCase() === "true" ? true : false
+}
+
 export default {
     CK: process.env.CK,
     CS: process.env.CS,
     PORT: process.env.PORT ?? 3000,
     ORIGIN: (process.env.ORIGIN ?? "https://localhost:3000") as string,
     COOKIE: { //このオブジェクトはcookeisのオプションにそのまま突っ込めるようになってる
-        httpOnly: process.env.C_HTTPONLY == undefined ? true : Boolean(process.env.C_HTTPONLY),
-        secure: process.env.C_SECURE == undefined ? true : Boolean(process.env.C_SECURE),
+        httpOnly: process.env.C_HTTPONLY == undefined ? true : boolean(process.env.C_HTTPONLY),
+        secure: process.env.C_SECURE == undefined ? true : boolean(process.env.C_SECURE),
         sameSite: process.env.C_SAMESITE as "strict" | "lax" | "none" ?? "strict",
         domain: process.env.C_DOMAIN,
         maxAge: process.env.C_MAXAGE == undefined ?  60 * 60 * 24 * 365 : Number(process.env.C_MAXAGE),
